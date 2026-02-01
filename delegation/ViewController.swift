259cdcc0e70f2df469078094bd16e91003931849
TIN PHAM <ptbtin22@clc.fitus.edu.vn>
2026-02-01 19:34:27 +0700

    Initial Commit

---
+++ delegation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegation
+//
+//  Created by Tin Pham on 1/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
